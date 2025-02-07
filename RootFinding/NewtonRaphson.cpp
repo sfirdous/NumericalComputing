9923624d8c6257f1653eaf37d7211a22018e5333
Shaikh Firdous <126231373+sfirdous@users.noreply.github.com>
2025-02-07 11:36:18 +0530

    Update NewtonRaphson.cpp

--- RootFinding/NewtonRaphson.cpp
+++ RootFinding/NewtonRaphson.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 void RootFinding::newtonRaphson()
 {
-    double x0 = findInitialGuess(); // Initial guess
-    cout << fixed << setprecision(4); // Set decimal precision to 6 places
+    double x0 = findInitialGuess(); 
+    cout << fixed << setprecision(4); 
     cout << "Iter |    x        |   f(x)      |   f'(x)     |   x_new     |  Diff  \n";
     cout << "------------------------------------------------------------------------\n";
 
@@ -16,20 +16,20 @@ void RootFinding::newtonRaphson()
         double fx = f(x0);
         double dfx = df(x0);
 
-        if (fabs(dfx) < 1e-6)
-        { // Avoid division by zero
+        if (fabs(dfx) < tol)
+        { 
             cout << "Derivative too small. Method failed.\n";
             return;
         }
 
-        x_new = x0 - (fx / dfx);        // Newton-Raphson formula
-        double diff = fabs(x_new - x0); // Compute difference between iterations
+        x_new = x0 - (fx / dfx);        
+        double diff = fabs(x_new - x0); 
 
         cout << iteration + 1 << "    | " << x0 << "  | " << fx << "  | "
              << dfx << "  | " << x_new << "  | " << diff << "\n";
 
         if (diff < tol)
-        { // Stop when difference is less than tolerance
+        { 
             break;
         }
 
@@ -38,5 +38,5 @@ void RootFinding::newtonRaphson()
     }
 
     cout << "------------------------------------------------------------------------\n";
-    cout << "Root Approximation: " << x_new << " (after " << iteration + 1 << " iterations)\n";
+    cout << "Newton Raphson Root Approximation: " << x_new << " (after " << iteration + 1 << " iterations)\n";
 }

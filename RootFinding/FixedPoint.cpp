19c8a47be4df848a30cfa9e28d8d4b4b79ee96b0
Shaikh Firdous <126231373+sfirdous@users.noreply.github.com>
2025-02-07 12:33:40 +0530

    Update FixedPoint.cpp

--- RootFinding/FixedPoint.cpp
+++ RootFinding/FixedPoint.cpp
@@ -1,26 +1,29 @@
 #include "RootFinding.hpp"
 using namespace std;
 
-void RootFinding::fixedPointIteration()
+double RootFinding::fixedPointIteration()
 {
-    double x0 = findInitialGuess(); // Initial guess
-    cout << fixed << setprecision(4);
-    cout << "Iter |    x        |   f(x)     |   Diff  \n";
+    double x0 = findInitialGuess(); 
+    cout << fixed << setprecision(6);
+    cout << "Iter |    x        |   f(x)     \n";
     cout << "--------------------------------------------\n";
 
     double x_new;
+    int iter = 0;
 
     do
     {
         x_new = g(x0);
-        cout << "Iteration-" << step << ":\t x1 = " << setw(10) << x_new << " and f(x1) = " << setw(10) << f(x_new) << endl;
+        cout << iter << "\t  "<< x_new << "\t  " << f(x_new) << endl;
 
-        step = step + 1;
+        iter = iter + 1;
 
         x0 = x_new;
 
     } while (fabs(f(x_new)) > tol);
 
     cout << "--------------------------------------------\n";
-    cout << "Fixed Point Approximation: " << x_new << " (after " << step << " iterations)\n";
+    cout << "Fixed Point Approximation: " << x_new << " (after " << iter << " iterations)\n";
+
+    return x_new;
 }

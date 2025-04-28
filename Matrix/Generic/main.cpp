a719b094c8febf42fcba959abc22a61caac564a0
Shaikh Firdous <126231373+sfirdous@users.noreply.github.com>
2025-04-28 22:02:29 +0530

    Update main.cpp

--- Matrix/Generic/main.cpp
+++ Matrix/Generic/main.cpp
@@ -2,10 +2,6 @@
 using namespace std;
 
 int main() {
-    // Matrix<int> A(2, 2);
-    // Matrix<int> C(A);
-    // std::cout << A << std::endl;
-    // std::cout << C << std::endl;
 
     Matrix<int> A(2,2);
     cout << A.getRow() << A.getCol() << endl;

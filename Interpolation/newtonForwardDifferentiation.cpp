dece0f102a81e2239c8e2fa900efb84bf76a6328
firdous <sfirdous0404@gmail.com>
2025-08-16 22:03:11 +0530

    Unable to resolve the issue

--- Interpolation/newtonForwardDifferentiation.cpp
+++ Interpolation/newtonForwardDifferentiation.cpp
@@ -1,19 +1,26 @@
 #include "Interpolation.hpp"
+#include "./Polynomial/Polynomial.hpp"
 
 void Interpolation::calculateFallingFactorial()
 {
-    P.resize(n);
+    P.resize(n) ; // store P through P[n]
 
-   std::vector<double> p = {0,1};
-    P[1] = Polynomial(p);
+    std::vector<double> temp = {1};
+    // Base case: (x)_0 = 1
+    P[0]= Polynomial(temp);
 
-    
-    for(int i = 2 ; i < n ; ++i)
-       { 
-        std::vector<double> p1 = {double(i-1),1};
+        std::vector<double> temp2 = {{0, 1}};
+
+    // (x)_1 = x
+    P[1] = Polynomial(temp2);
+
+    // General case
+    for(int i = 2; i < n; ++i)
+    { 
+        // Polynomial for (x - (i-1))
+        std::vector<double> p1 = { -double(i-1), 1 };
+
         Polynomial poly = Polynomial(p1);
-
-        P[i] = P[i-1].mul(poly);}
-
+        P[i] = P[i-1].mul(poly);
+    }
 }
-

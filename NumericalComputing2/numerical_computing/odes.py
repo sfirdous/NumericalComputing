0a26a4eb1be0c4a80221f1fb7da0ffdd857a7d92
firdous <sfirdous0404@gmail.com>
2025-10-26 16:48:24 +0530

    shifted the fuction of h Vs error in odes.py

--- NumericalComputing2/numerical_computing/odes.py
+++ NumericalComputing2/numerical_computing/odes.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 class ODES:
     def __init__(self,function,y0,h,a,b):
@@ -34,4 +35,47 @@ class ODES:
             self.y[i+1] = self.y[i] + (1/6)*(k1+2*k2+2*k3+k4) 
             
         
+def plot_h_vs_error():
+    h = np.float32(1)
+    a = 0
+    b = 2
+
+    step_size = np.array([],dtype = np.float32)
+    error_euler = np.array([],dtype = np.float32)
+    error_mod = np.array([],dtype = np.float32)
+    error_rk4 = np.array([],dtype = np.float32)
+
+    y = "math.exp(-2*x)+ (1/3)*math.exp(-x)"
+    y_prime = "-2*y+math.exp(-x)"
         
+    
+    
+    for i in range(0,11):
+        actual_y = eval(y,{"x":b,"math":math})
+        
+        obj = ODES(y_prime,1,h,a,b)
+        
+        obj.euler()
+        error = abs(actual_y - obj.y[-1])
+        error_euler = np.append(error_euler,error)
+        
+        obj.mod_euler()
+        error = abs(actual_y - obj.y[-1])
+        error_mod = np.append(error_mod,obj.y[-1])
+        
+        obj.runge_kutta4()
+        error = abs(actual_y - obj.y[-1])
+        error_rk4 = np.append(error_rk4,obj.y[-1])
+                
+        step_size = np.append(step_size,h)
+        h = h*0.5
+        
+        
+    plt.plot(step_size,error_euler,label = 'Euler')
+    plt.plot(step_size,error_mod,label = 'Mod Euler')
+    plt.plot(step_size,error_rk4,label = 'Runge Kutta 4')
+    plt.xlabel('Step size (h)')    
+    plt.ylabel('Error')   
+    plt.title('Step Size VS Error')
+    plt.legend()
+    plt.show()
